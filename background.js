bb953ceb34def98cc5a5fe9131a546d8ab7d0670
Ian Izaguirre <ian@izaguir.re>
2018-10-08 23:38:06 -0400

    Add: user interface and declarativeContent API

--- background.js
+++ background.js
@@ -1,5 +1,25 @@
+// chrome.runtime.onInstalled.addListener(function() {
+//   chrome.storage.sync.set({color: '#46a5e5'},
+//   function() {
+//     console.log('The color is Blue');
+//   }
+//   )
+// })
+
 chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.set({ color: '#46a5e5' }, function() {
     console.log('The color is Blue');
+  });
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { hostEquals: 'developer.chrome.com' }
+          })
+        ],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+      }
+    ]);
   });
 });
