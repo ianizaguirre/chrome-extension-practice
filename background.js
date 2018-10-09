0c00085182f64d05a8a808e826a27be11dfdd81b
Ian Izaguirre <ian@izaguir.re>
2018-10-09 00:40:11 -0400

    Add: comments

--- background.js
+++ background.js
@@ -1,15 +1,13 @@
-// chrome.runtime.onInstalled.addListener(function() {
-//   chrome.storage.sync.set({color: '#46a5e5'},
-//   function() {
-//     console.log('The color is Blue');
-//   }
-//   )
-// })
-
+// ---- Part1 ----
 chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.set({ color: '#46a5e5' }, function() {
     console.log('The color is Blue');
   });
+
+  // ---- Part2 ----
+  // It is up to the extension to tell the browser when the user can interact with popup.html.
+
+  // --> Use declarative injection for content scripts that should be automatically run on SPECIFIED  pages.
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([
       {
--- popup.js
+++ popup.js
@@ -1,15 +1,20 @@
 let changeColor = document.getElementById('changeColor');
 
+// ---- Part1 ----
+// Grab the pre-defined css color of our button
+// This code grabs the button from popup.html and requests the color value from storage. It then applies THAT color as the background of the button.
 chrome.storage.sync.get('color', function(data) {
   changeColor.style.backgroundColor = data.color;
   changeColor.setAttribute('value', data.color);
 });
 
-// ----
-
+// ---- Part2 ----
+// On Button Click --> trigger a programatically injected content script:
 changeColor.onclick = function(element) {
   let color = element.target.value;
   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    // Execute Script
+    // This turns the background color of the page the same color as the button
     chrome.tabs.executeScript(tabs[0].id, { code: 'document.body.style.backgroundColor = "' + color + '";' });
   });
 };
